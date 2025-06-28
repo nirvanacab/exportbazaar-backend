f4303da112defd65ad50fffa56c728b1b06adfc2
nirvanacab <nirvanacab@gmail.com>
2025-06-28 19:11:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ app.use(cors({
 app.use(express.json());
 
 // MySQL connection using env variables
-const db = mysql.createConnection({
+const db = mysql.createPool({
+  connectionLimit: 10,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
+
 
 db.connect(err => {
   if (err) {
